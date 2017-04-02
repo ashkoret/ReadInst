0ead3840fb8191f7f5490bdabeef510171a6f9f1
shelest <el.rache@gmail.com>
2017-04-02 16:38:42 +0200

    Add AppConfig and beginning of DBConnect

---
+++ app/src/main/java/com/readinst/dbconnector/DBconnect.java
@@ -0,0 +1,37 @@
+package com.readinst.dbconnector;
+
+import com.readinst.readinst.AppConfig;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import android.util.Log;
+
+/**
+ * Created by dan on 02-04-17.
+ */
+
+public class DBconnect {
+    private Connection conn;
+    private String TAG = DBconnect.class.getSimpleName();
+
+    public DBconnect()
+    {
+        conn = null;
+        try
+        {
+            Class.forName(AppConfig.DRIVER);
+            conn = DriverManager.getConnection(AppConfig.connectionString, AppConfig.db_user, AppConfig.db_pass);
+        }
+        catch(SQLException s)
+        {
+            Log.e(TAG, s.getMessage());
+        }
+        catch(ClassNotFoundException c)
+        {
+            Log.e(TAG, c.getMessage());
+        }
+    }
+
+}
