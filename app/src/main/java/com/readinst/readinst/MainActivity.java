36d2ce0b9ea8aecf8566bf3d8ac51d4df95b53cb
Denis <el.rache@gmail.com>
2017-04-06 16:09:00 +0200

    Implement file write

--- app/src/main/java/com/readinst/readinst/MainActivity.java
+++ app/src/main/java/com/readinst/readinst/MainActivity.java
@@ -1,10 +1,13 @@
 package com.readinst.readinst;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import 	android.os.StrictMode;
+import android.os.StrictMode;
 import android.widget.Button;
 import android.view.View;
 import android.widget.TextView;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileWriter;
 
 import com.readinst.dbconnector.DBconnect;
 import org.springframework.security.crypto.bcrypt.BCrypt;
@@ -29,14 +32,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-
         UserEmail = (TextView) findViewById(R.id.Email);
         String Email = UserEmail.getText().toString();
         Password = (TextView) findViewById(R.id.Password);
         String Pass = Password.getText().toString();
+        String UserString  = Email + "--"+Pass;
+        try {
+            File file = new File(this.getFilesDir(), "current.usr");
+            FileWriter writer = new FileWriter(file);
+            writer.append(UserString);
+            writer.flush();
+            writer.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
         // TODO implement login procedure
 
         DBconnect db_users = new DBconnect();
-        db_users.insertUser(Email, Pass, BCrypt.gensalt(12),"27FA-AE34-1FC5-EECA-4E48-D19D-DFBA-4B68", AppConfig.TABLE_USERS);
+        db_users.insertUser(Email, Pass, BCrypt.gensalt(12),"null", AppConfig.TABLE_USERS);
+
     }
 }
