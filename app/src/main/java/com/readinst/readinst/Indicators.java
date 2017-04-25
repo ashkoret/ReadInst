15b673d1507816577ae1c125ddb156492333d184
shelest <el.rache@gmail.com>
2017-04-25 22:50:17 +0200

    Add menu

--- app/src/main/java/com/readinst/readinst/Indicators.java
+++ app/src/main/java/com/readinst/readinst/Indicators.java
@@ -2,12 +2,42 @@ package com.readinst.readinst;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Indicators extends AppCompatActivity {
+
+    //TODO Add menu
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_indicators);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.AddPC:
+                Toast.makeText(Indicators.this, "Add PC using QR", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.AddPCman:
+                Toast.makeText(Indicators.this, "Add PC manually", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.Exit:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
