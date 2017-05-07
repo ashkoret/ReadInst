9b2e0600205bd0acb39b9b612232ca737d86bba4
shelest <el.rache@gmail.com>
2017-05-07 20:26:20 +0200

    Add QR-reader classes, remove Jack and 1.8 compiler support. Compiled on 2.4. Studio Preview.

---
+++ app/src/main/java/com/readinst/barcode/BarcodeTracker.java
@@ -0,0 +1,28 @@
+package com.readinst.barcode;
+
+/**
+ * Created by dan on 07-05-17.
+ */
+import android.content.Context;
+
+import com.google.android.gms.vision.Tracker;
+import com.google.android.gms.vision.barcode.Barcode;
+
+class BarcodeTracker extends Tracker<Barcode> {
+    private BarcodeGraphicTrackerCallback mListener;
+
+    public interface BarcodeGraphicTrackerCallback {
+        void onDetectedQrCode(Barcode barcode);
+    }
+
+    BarcodeTracker(Context listener) {
+        mListener = (BarcodeGraphicTrackerCallback) listener;
+    }
+
+    @Override
+    public void onNewItem(int id, Barcode item) {
+        if (item.displayValue != null) {
+            mListener.onDetectedQrCode(item);
+        }
+    }
+}
